93b94962a731737868b45560449ddef9208c06e6
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-12-01 14:19:40 -0500

    Tercera parte: Enlazar datos a la interfaz de usuario y navegar por páginas. COMPLETADA

--- JoaoConde-TallerApuntes/Views/NotePage.xaml.cs
+++ JoaoConde-TallerApuntes/Views/NotePage.xaml.cs
@@ -1,4 +1,4 @@
-namespace JoaoConde_TallerApuntes;
+namespace JoaoConde_TallerApuntes.Views;
 
 public partial class NotePage : ContentPage
 {
@@ -8,8 +8,15 @@ public partial class NotePage : ContentPage
 	{
 		InitializeComponent();
 
+        /*
         if (File.Exists(_fileName))
             TextEditor.Text = File.ReadAllText(_fileName);
+        */
+
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
 
     private void SaveButton_Clicked(object sender, EventArgs e)
@@ -25,5 +32,19 @@ public partial class NotePage : ContentPage
             File.Delete(_fileName);
 
         TextEditor.Text = string.Empty;
+    }
+
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
 }
